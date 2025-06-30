fc3e7f6d7443639eb9c2318ba7af3d37b37b84c3
Mawen Salignat-Moandal <mwnslgt@gmail.com>
2025-06-30 17:13:58 +0200

    add Component for Online Status

---
+++ frontend/components/OnlineStatusBanner.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useOnlineStatus } from "@/hook/useOnlineStatus"; // ou selon ton chemin
+
+export function OnlineStatusBanner() {
+  const isOnline = useOnlineStatus();
+
+  return (
+    <div>
+      {!isOnline && <div className="bg-yellow-200 p-2">Mode hors-ligne</div>}
+    </div>
+  );
+}
